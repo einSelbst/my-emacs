e0b6d2f2502486a7a7acf474e22b7c41b4896d91
Silvio <silvio@sintuity.com>
2019-12-19 00:24:58 +0100

    Add some more defaults

--- init.el
+++ init.el
@@ -1,12 +1,48 @@
 ;; einSelbst emacs init file
 ;; https://github.com/einSelbst/my-emacs
 
-;; load dedicated custom.el
-;; https://github.com/angrybacon/dotemacs/blob/master/dotemacs.org#load-customel
+;; set defaults
+;; https://github.com/angrybacon/dotemacs/blob/master/dotemacs.org#use-better-defaults
 
-(setq-default custom-file (expand-file-name "custom.el" user-emacs-directory))
-(when (file-exists-p custom-file)
-  (load custom-file))
+(setq-default
+ ;; ad-redefinition-action 'accept                   ; Silence warnings for redefinition
+ ;; auto-window-vscroll nil                          ; Lighten vertical scroll
+ ;; confirm-kill-emacs 'yes-or-no-p                  ; Confirm before exiting Emacs
+ ;; cursor-in-non-selected-windows nil               ; Hide the cursor in inactive windows
+ delete-by-moving-to-trash t                      ; Delete files to trash
+ ;; display-time-default-load-average nil            ; Don't display load average
+ ;; display-time-format "%H:%M"                      ; Format the time string
+ fill-column 80                                   ; Set width for automatic line breaks
+ help-window-select t                             ; Focus new help windows when opened
+ indent-tabs-mode nil                             ; Stop using tabs to indent
+ inhibit-startup-screen t                         ; Disable start-up screen
+ initial-scratch-message ""                       ; Empty the initial *scratch* buffer
+ mouse-yank-at-point t                            ; Yank at point rather than pointer
+ ;; ns-use-srgb-colorspace nil                       ; Don't use sRGB colors
+ recenter-positions '(5 top bottom)               ; Set re-centering positions
+ scroll-conservatively most-positive-fixnum       ; Always scroll by one line
+ scroll-margin 10                                 ; Add a margin when scrolling vertically
+ select-enable-clipboard t                        ; Merge system's and Emacs' clipboard
+ sentence-end-double-space nil                    ; End a sentence after a dot and a space
+ show-trailing-whitespace nil                     ; Display trailing whitespaces
+ tab-width 4                                      ; Set width for tabs
+ uniquify-buffer-name-style 'forward              ; Uniquify buffer names
+ window-combination-resize t                      ; Resize windows proportionally
+ x-stretch-cursor t                               ; Stretch cursor to the glyph width
+ 
+ custom-file (expand-file-name "custom.el" user-emacs-directory))  ; load dedicated custom.el
+;; https://github.com/angrybacon/dotemacs/blob/master/dotemacs.org#load-customel
+(when (file-exists-p custom-file)(load custom-file))
+
+(cd "~/")                                         ; Move to the user directory
+(delete-selection-mode 1)                         ; Replace region when inserting text
+(fset 'yes-or-no-p 'y-or-n-p)                     ; Replace yes/no prompts with y/n
+(global-subword-mode 1)                           ; Iterate through CamelCase words
+(mouse-avoidance-mode 'banish)                    ; Avoid collision of mouse with point
+(put 'downcase-region 'disabled nil)              ; Enable downcase-region
+(put 'upcase-region 'disabled nil)                ; Enable upcase-region
+(set-default-coding-systems 'utf-8)               ; Default to utf-8 encoding
+
 
 ;; straight.el package manager
 ;; https://github.com/raxod502/straight.el
