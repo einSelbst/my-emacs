ff4ea9f633b8a56f7d9fc2b9bed4bdc7bd6cdad8
Silvio <silvio@sintuity.com>
2020-08-07 01:12:45 +0200

    Add License and Metadata

--- init.el
+++ init.el
@@ -1,7 +1,39 @@
-;;; package init - Summary,
+;;; init.el --- emacs config for (m)einSelbst
+
+;; Copyright © 2013-2020 Phil Hagelberg and contributors
+
+;; Author: einSelbst
+;; URL: ;; https://github.com/einSelbst/my-emacs
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "27.1"))
+;; Created: 2020-07-01
+;; Keywords: convenience
+
+;; This file is NOT part of GNU Emacs.
+
 ;;; Commentary:
+
 ;; einSelbst Emacs init file
-;; https://github.com/einSelbst/my-emacs
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
 
 ;; set defaults
 ;; https://github.com/angrybacon/dotemacs/blob/master/dotemacs.org#use-better-defaults
