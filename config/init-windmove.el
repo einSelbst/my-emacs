aebbe85644052e90765096dd6a733e3c17972442
Silvio <silvio@sintuity.com>
2020-08-07 20:36:51 +0200

    Update init-windmove.el

--- config/init-windmove.el
+++ config/init-windmove.el
@@ -1,4 +1,7 @@
-;;; #init-windmove.el
+;;; init-windmove.el --- let me easily switch buffers
+;;; Commentary:
+;;; Code:
+(require 'use-package)
 
 (use-package windmove
   :init
@@ -19,24 +22,29 @@
 ;; https://batsov.com/articles/2012/03/08/emacs-tip-number-5-save-buffers-automatically-on-buffer-or-window-switch/
 ;; automatically save buffers associated with files on buffer switch
 ;; and on windows switch
-(defadvice switch-to-buffer (before save-buffer-now activate)
-  (when (and buffer-file-name (buffer-modified-p)) (save-buffer)))
-(defadvice other-window (before other-window-now activate)
-  (when (and buffer-file-name (buffer-modified-p)) (save-buffer)))
-(defadvice windmove-up (before other-window-now activate)
-  (when (and buffer-file-name (buffer-modified-p)) (save-buffer)))
-(defadvice windmove-down (before other-window-now activate)
-  (when (and buffer-file-name (buffer-modified-p)) (save-buffer)))
-(defadvice windmove-left (before other-window-now activate)
-  (when (and buffer-file-name (buffer-modified-p)) (save-buffer)))
-(defadvice windmove-right (before other-window-now activate)
-  (when (and buffer-file-name (buffer-modified-p)) (save-buffer)))
+;; (defadvice switch-to-buffer (before save-buffer-now activate)
+;;   (when (and buffer-file-name (buffer-modified-p)) (save-buffer)))
+;; (defadvice other-window (before other-window-now activate)
+;;   (when (and buffer-file-name (buffer-modified-p)) (save-buffer)))
+;; (defadvice windmove-up (before other-window-now activate)
+;;   (when (and buffer-file-name (buffer-modified-p)) (save-buffer)))
+;; (defadvice windmove-down (before other-window-now activate)
+;;   (when (and buffer-file-name (buffer-modified-p)) (save-buffer)))
+;; (defadvice windmove-left (before other-window-now activate)
+;;   (when (and buffer-file-name (buffer-modified-p)) (save-buffer)))
+;; (defadvice windmove-right (before other-window-now activate)
+;;   (when (and buffer-file-name (buffer-modified-p)) (save-buffer)))
+
 
 ;; saves the buffers if I de-focus emacs completely (moving to another screen)
-(add-hook 'focus-out-hook
-          (lambda () (flet ((message
-                             (format &rest args) nil))
-                           (save-some-buffers t))))
+;; (add-hook 'focus-out-hook
+;; (add-hook 'after-focus-change-hook
+          ;; (save-some-buffers t))
+
+;; (add-hook 'after-focus-change-hook
+;;           (lambda () (flet ((message
+;;                              (format &rest args) nil))
+;;                        (save-some-buffers t))))
 
 (provide 'init-windmove)
-;;; init-windmove.el ends here.
+;;; init-windmove.el ends here
