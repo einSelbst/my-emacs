1d50c8d00f33c730fb00fabd376ad135443fdc22
einSelbst <silvio@mailbox.org>
2021-05-03 10:36:51 +0200

    remap mac modifiers, not sure if it will conflict on other machines

--- custom.el
+++ custom.el
@@ -7,10 +7,11 @@
    ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
  '(cua-mode t nil (cua-base))
  '(custom-safe-themes
-   (quote
-    ("c086fe46209696a2d01752c0216ed72fd6faeabaaaa40db9fc1518abebaf700d" "846ef3695c42d50347884515f98cc359a7a61b82a8d0c168df0f688cf54bf089" default)))
+   '("c086fe46209696a2d01752c0216ed72fd6faeabaaaa40db9fc1518abebaf700d" "846ef3695c42d50347884515f98cc359a7a61b82a8d0c168df0f688cf54bf089" default))
  '(global-display-line-numbers-mode t)
- '(safe-local-variable-values (quote ((flycheck-disabled-checkers emacs-lisp-checkdoc))))
+ '(mac-command-modifier 'super)
+ '(mac-option-modifier 'meta)
+ '(safe-local-variable-values '((flycheck-disabled-checkers emacs-lisp-checkdoc)))
  '(save-place-mode t)
  '(show-paren-mode t)
  '(tool-bar-mode nil)
