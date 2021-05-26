c7bb7956e69e3e32ed970b6ad8daaea683cf8b84
Silvio <silvio@mailbox.org>
2021-05-26 10:03:18 +0200

    A few updates to setup-ui

--- config/setup-ui.el
+++ config/setup-ui.el
@@ -5,12 +5,32 @@
 
 (use-package all-the-icons)
 
+(use-package all-the-icons-dired
+  :config
+  (add-hook 'dired-mode-hook 'all-the-icons-dired-mode))
+
+(use-package all-the-icons-ibuffer)
+
+;; (use-package vscode-dark-plus-theme
+;;   :config
+;;   (load-theme 'vscode-dark-plus t))
+
+;; (use-package vscode-icon
+;;   :ensure t
+;;   :commands (vscode-icon-for-file))
+
+;; (use-package zerodark-theme
+;;   :init (load-theme 'zerodark t)
+;;   :custom
+;;   (zerodark-setup-modeline-format)
+;;   )
+
 (use-package doom-themes
+  :after all-the-icons
   :config
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-moonlight t)
 
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
@@ -18,14 +38,17 @@
   ;; Enable custom neotree theme (all-the-icons must be installed!)
   ;; (doom-themes-neotree-config)
   ;; or for treemacs users
-  (setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
+  (doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
   (doom-themes-treemacs-config)
   
   ;; Corrects (and improves) org-mode's native fontification.
-  ;; (doom-themes-org-config)
+  (doom-themes-org-config)
   )
 
-(load-theme 'doom-moonlight t)
+(load-theme 'doom-moonlight t) ;; this is needed altough it shouldn't
+
+(use-package highlight-escape-sequences
+  :hook (prog-mode . hes-mode))
 
 (use-package golden-ratio
   :defer t
