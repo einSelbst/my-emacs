941b12af7a9c3245686e993ff08c001bf2587fe5
einSelbst <silvio@mailbox.org>
2022-09-22 12:55:24 +0200

    who did this???

--- config/setup-ui.el
+++ config/setup-ui.el
@@ -31,9 +31,9 @@
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-
+  :custom
   ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
+  ((doom-themes-visual-bell-config)
   
   ;; Enable custom neotree theme (all-the-icons must be installed!)
   ;; (doom-themes-neotree-config)
@@ -44,7 +44,7 @@
   
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config)
-  )
+  ))
 
 (load-theme 'doom-moonlight t) ;; this is needed altough it shouldn't
 
